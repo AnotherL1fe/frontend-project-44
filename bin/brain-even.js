3927a4e9ed059407f167fec9a8c0615274d5e4fb
AnotherL1fe <retributionanotherlife@gmail.com>
2025-06-05 14:33:44 +0300

    Проверил через линтер

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-// ↑ Шебанг (обязательно!) — указывает, что скрипт запускается через Node.js
 
 import { isEven, generationNumber } from '../src/helpers/helpers.js'
 import runGame from '../src/runGame.js'
