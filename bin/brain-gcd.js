8c10cfeddc8d5f7804fe6c0a81812cd770345f30
AnotherL1fe <retributionanotherlife@gmail.com>
2025-06-05 14:20:43 +0300

    Обновил brain-gcd.js (Убрал лишнию функцию приветствия)

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -16,4 +16,4 @@ const generateGcdQuestion = () => {
 }
 
 const description = `Find the greatest common divisor of given numbers.`
-runGame(description, generateGcdQuestion)
+runGame(description, generateGcdQuestion);
